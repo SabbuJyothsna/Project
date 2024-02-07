850a654354718b916b75c1eb40371a91d76e55e7
dnyaneshwari-09 <dnyaneshwaridhage418@gmail.com>
2024-02-07 21:58:11 +0530

    components added

--- messportal/src/App.js
+++ messportal/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import VendorsComp from './Components/VendorsComp';
+import IndividualVendorComp from './Components/IndividualVendorComp';
+import CustomerDeliveryComp from './Components/CustomerDeliveryComp';
+import RouterComp from './Components/RouterComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      {/* <VendorsComp/> */}
+      {/* <IndividualVendorComp/> */}
+
+      {/* <CustomerDeliveryComp/> */}
+
+      <RouterComp/>
+
     </div>
   );
 }
